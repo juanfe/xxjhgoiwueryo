525f1c69d04961c1b586993e865fdc34223b92bf
NAME <cmartinez@vichara.com>
2012-02-05 17:14:02 +0000

    Adding the integration project ls

---
+++ ls/source/class/ls/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("ls.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ ls/source/class/ls/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("ls.theme.Theme",
+{
+  meta :
+  {
+    color : ls.theme.Color,
+    decoration : ls.theme.Decoration,
+    font : ls.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : ls.theme.Appearance
+  }
+});
