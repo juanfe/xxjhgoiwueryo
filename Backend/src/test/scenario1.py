37832af1a1231257af88e72ff436a262a7d416fc
jjaramillo <jjaramillo@vichara.com>
2012-02-27 16:40:25 -0500

    Removing unused value in parameters

--- Backend/src/test/scenario1.py
+++ Backend/src/test/scenario1.py
@@ -1,7 +1,7 @@
 import datetime
 
 argument = ['LiqSpot.py', '-b', '../../sample/bids1.csv', '-l', '../../sample/loans1.csv',
-		'True', '-d', ',', '-M', '../mo.csv', '-o', 'test.csv']
+		'-d', ',', '-M', '../mo.csv', '-o', 'test.csv']
 
 scenario = {'test_init' : '../mo.csv',
 		'test_LoadMortgageOperators' :
