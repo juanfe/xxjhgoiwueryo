4ca62a4648a6df60f897e96bd1715c4c059a08ee
jjaramillo <jjaramillo@vichara.com>
2012-03-08 09:41:13 -0500

    Change the default rate in the test_addLoans and test_LoadLoans [ref #3]

--- Backend/src/test/scenario0.py
+++ Backend/src/test/scenario0.py
@@ -1,22 +1,22 @@
 import datetime
 
 argument = ['LiqSpot.py', '-b', '../../sample/bids0.csv', '-l', '../../sample/loans0.csv',
-		'-d', ';', '-M', '../mo.csv', '-o', 'test.csv']
+		'-d', ';', '-M', '../mo.csv', '-o', 'test.csv', '-R', 1]
 
 scenario = {'test_init' : '../mo.csv', 
 		'test_LoadMortgageOperators' :
 			['ABC Mortgage', 'Prime Lending', 'Best Loans Inc', 'Integrity Lending'],
 		'test_addLoans' : { 'p' : ['MO', 'Load Amount', 'Rate'],
-			'q' : ['ABC Mortgage', ' 318725', ' 0.0225'],
-			'r' : [{'MO': 'ABC Mortgage', 'Load Amount': 318725.0, 'Rate': 0.0225}],
+			'q' : ['ABC Mortgage', ' 318725', '0.01'],
+			'r' : [{'MO': 'ABC Mortgage', 'Load Amount': 318725.0, 'Rate': 0.01}],
 			},
 		'test_LoadLoans' : 
-			[{'MO': 'ABC Mortgage', 'Load Amount': 318725.0, 'Rate': 0.0225},
-				{'MO': 'ABC Mortgage', 'Load Amount': 375685.0, 'Rate': 0.0225},
-				{'MO': 'Prime Lending', 'Load Amount': 479875.0, 'Rate': 0.03},
-				{'MO': 'Prime Lending', 'Load Amount': 525400.0, 'Rate': 0.06},
-				{'MO': 'Best Loans Inc', 'Load Amount': 515425.0, 'Rate': 0.025},
-				{'MO': 'Integrity Lending', 'Load Amount': 485000.0, 'Rate': 0.06},
+			[{'MO': 'ABC Mortgage', 'Load Amount': 318725.0, 'Rate': 0.01},
+				{'MO': 'ABC Mortgage', 'Load Amount': 375685.0, 'Rate': 0.01},
+				{'MO': 'Prime Lending', 'Load Amount': 479875.0, 'Rate': 0.01},
+				{'MO': 'Prime Lending', 'Load Amount': 525400.0, 'Rate': 0.01},
+				{'MO': 'Best Loans Inc', 'Load Amount': 515425.0, 'Rate': 0.01},
+				{'MO': 'Integrity Lending', 'Load Amount': 485000.0, 'Rate': 0.01},
 				{'MO': 'Total', 'Load Amount': 2700110.0}],
 		'test_LoadBids' : { 'p' : 20,
 			'q' : '1104154',
