5fbe77eb1429f705b7ee4c992f0e1bc37c71c4a2
jjaramillo <jjaramillo@vichara.com>
2012-04-10 11:29:37 -0500

    Removing parameters from sample's scripts [ref #5]

--- Backend/sample/l3.sh
+++ Backend/sample/l3.sh
@@ -1,1 +1,1 @@
-../src/LiqSpot.py -b bids3.csv -u users3.csv -l loans3.csv -d "," -o test.csv -r -A -R 3.5 -v
+../src/LiqSpot.py -b bids3.csv -u users3.csv -l loans3.csv -d "," -o test.csv -R 3.5 -v
