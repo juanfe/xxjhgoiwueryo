2a69b8e88c8e3a46c89a8470c245082f1dbb9196
jjaramillo <jjaramillo@vichara.com>
2012-04-19 10:59:11 -0500

    Adding initial loans app [ref #7]

---
+++ Web/loans/models.py
@@ -0,0 +1,16 @@
+import moneyed
+from djmoney.models.fields import MoneyField
+from django.db import models
+
+class MortgageOriginator(models.Model):
+	Name = models.CharField(max_length=200)
+	#Contact = models.ForeignKey()
+
+class Loans(models.Model):
+	MortgageOriginator = models.ForeignKey(MortgageOriginator)
+	Amount = MoneyField(max_digits = 20, decimal_places = 9,
+			default_currency=moneyed.USD)
+	AmountDate = models.DateTimeField(auto_now=True)
+	Creation = models.DateTimeField(auto_now=True)
+	Assignation = models.DateTimeField()
+	Hiden = models.BooleanField()
