73c4b5e07e008dbc46b99a48475e13f20e827b4d
jjaramillo <jjaramillo@vichara.com>
2012-04-23 11:51:35 -0500

    Adding money version from https://github.com/igloox/python-money.git

--- Web/loans/views.py
+++ Web/loans/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("An application from Vichara Technologies.")
+
+def detail(request, loan_id):
+	return HttpResponse("You are at loan %s." % loan_id)
+
+def results(request, loan_id):
+	return HttpResponse("You are at result of loan %s." % loan_id)
+
+
--- Web/urls.py
+++ Web/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     # url(r'^$', 'Web.views.home', name='home'),
-    # url(r'^Web/', include('Web.foo.urls')),
-
+    url(r'^loans/$', 'loans.views.index'), 
+    url(r'^loans/(?P<loan_id>\d+)/$', 'loans.views.detail'),
+    url(r'^loans/(?P<loan_id>\d+)/results/$', 'loans.views.results'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
