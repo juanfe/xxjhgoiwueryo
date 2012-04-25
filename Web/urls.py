b5073f199cf7d1f5a9cda111f12f00785f45182c
jjaramillo <jjaramillo@vichara.com>
2012-04-25 15:55:15 -0500

    Adding Mortgage Operator index page [ref #7]

--- Web/urls.py
+++ Web/urls.py
@@ -7,11 +7,12 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     #('^admin/', include(admin.site.urls)),
+    url(r'^mo/$', 'loans.views.moindex'), 
     url(r'^loans/$', 'loans.views.index'), 
     url(r'^loans/(?P<loan_id>\d+)/$', 'loans.views.detail'),
     url(r'^loans/(?P<loan_id>\d+)/results/$', 'loans.views.results'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     ('^$', 'django.views.generic.simple.direct_to_template',
-    {'template': 'home.html'}),
+        {'template': 'home.html'}),
 )
