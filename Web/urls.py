7454e52c30b45d3a71416c3ca82e912e7d3071f8
jjaramillo <jjaramillo@vichara.com>
2012-04-25 12:11:59 -0500

    Adding change to aggre with django-nonrel [ref #7]

--- Web/urls.py
+++ Web/urls.py
@@ -1,12 +1,17 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+handler500 = 'djangotoolbox.errorviews.server_error'
+
 urlpatterns = patterns('',
-    # url(r'^$', 'Web.views.home', name='home'),
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    #('^admin/', include(admin.site.urls)),
     url(r'^loans/$', 'loans.views.index'), 
     url(r'^loans/(?P<loan_id>\d+)/$', 'loans.views.detail'),
     url(r'^loans/(?P<loan_id>\d+)/results/$', 'loans.views.results'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    ('^$', 'django.views.generic.simple.direct_to_template',
+    {'template': 'home.html'}),
 )
