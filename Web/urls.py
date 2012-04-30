33c2884e71a9a9f6564544c44a1f0033fb8e31fa
jjaramillo <jjaramillo@vichara.com>
2012-04-30 09:21:46 -0500

    Change space and remove loans urls [ref #7]

--- Web/urls.py
+++ Web/urls.py
@@ -2,19 +2,22 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-handler500 = 'djangotoolbox.errorviews.server_error'
+#handler500 = 'djangotoolbox.errorviews.server_error'
+handler500 = 'templates.500.html'
+handler404 = 'templates.404.html'
+handler403 = 'templates.403.html'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    #('^admin/', include(admin.site.urls)),
     url(r'^mo/$', 'loans.views.moindex'), 
-    url(r'^loans/$', 'loans.views.index'), 
-    url(r'^loans/(?P<loan_id>\d+)/$', 'loans.views.detail'),
-    url(r'^loans/(?P<loan_id>\d+)/results/$', 'loans.views.results'),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^loans/', include('loans.urls')), 
+    url(r'^bids/', include('bids.urls')),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^myapp/', include('myapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^initial_data/$', include('initial_data')),
-	url(r'^dojango/', include('dojango.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^initial_data/$', include('initial_data')),
+    url(r'^dojango/', include('dojango.urls')),
     ('^$', 'django.views.generic.simple.direct_to_template',
         {'template': 'home.html'}),
 )
