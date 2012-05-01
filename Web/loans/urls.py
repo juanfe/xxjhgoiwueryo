5ab00b98a3256b74d745d5323d1d620c6aa1ec43
jjaramillo <jjaramillo@vichara.com>
2012-04-30 20:50:44 -0500

    Adding url to initial data [ref #7]

---
+++ Web/loans/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    url(r'^$', 'index'), 
+    url(r'^initial_data/$', 'initial_data'),
+    #url(r'^(?P<loan_id>\d+)/$', 'loans.views.detail'),
+    #url(r'^(?P<loan_id>\d+)/results/$', 'loans.views.results'),
+)
