389606e119afebac33ca089a2c2affc6e99b76bc
jjaramillo <jjaramillo@vichara.com>
2012-05-01 22:48:39 -0500

    Adding bid list [ref #7]

--- Web/bids/models.py
+++ Web/bids/models.py
@@ -4,36 +4,44 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Bid(models.Model):
-	TYPE_CHOICES = (
-			('G', 'General'),
-			('S', 'Specified'),
-	)
-	LOAN_CHOICES = (
-			('M', 'Mortgage Originator'),
-			('L', 'Loan'),
-	)
-	ORDER_CHOICES = (
-			('A', 'Auto'),
-			('D', 'Day Trade'),
-	)
-	STATUS_CHOICES = (
-			('K', 'Accepted'),
-			('A', 'Active'),
-			('C', 'Cancelled'),
-	)
-	User = models.ForeignKey(User)
-	CreatedAt = models.DateTimeField(auto_now = True)
-	ExpiresAt = models.DateTimeField(auto_now = True)
-	Type = models.CharField(max_length = 1, choices = TYPE_CHOICES)
-	Aggregated = MoneyField(max_digits = 20, decimal_places = 9,
-			default_currency = moneyed.USD)
-	AggregatedDate = models.DateTimeField(auto_now = True)
-	Participation = models.DecimalField(max_digits = 13, decimal_places = 9)
-	LoanType = models.CharField(max_length = 1, choices = LOAN_CHOICES)
-	Competitive = models.BooleanField()
-	CompetitiveBidRate = models.DecimalField(max_digits = 13, decimal_places = 9)
-	OrderTiming = models.CharField(max_length = 1, choices = ORDER_CHOICES)
-	FundsAvailable = MoneyField(max_digits = 20, decimal_places = 9,
-			default_currency = moneyed.USD)
-	FundsAvailableDate = models.DateTimeField(auto_now = True)
-	Status = models.CharField(max_length = 1, choices = STATUS_CHOICES)
+    TYPE_CHOICES = (
+            ('G', 'General'),
+            ('S', 'Specified'),
+    )
+    LOAN_CHOICES = (
+            ('M', 'Mortgage Originator'),
+            ('L', 'Loan'),
+    )
+    ORDER_CHOICES = (
+            ('A', 'Auto'),
+            ('D', 'Day Trade'),
+    )
+    STATUS_CHOICES = (
+            ('K', 'Accepted'),
+            ('A', 'Active'),
+            ('C', 'Cancelled'),
+    )
+    #User = models.ForeignKey(User)
+    User = models.CharField(max_length = 40)
+    CreatedAt = models.DateTimeField(auto_now = True)
+    ExpiresAt = models.DateTimeField(auto_now = True, null = True)
+    Type = models.CharField(max_length = 1, choices = TYPE_CHOICES)
+    #Aggregated = MoneyField(max_digits = 20, decimal_places = 9,
+    #            default_currency = moneyed.USD)
+    Aggregated = models.FloatField(verbose_name="Aggregate $", null = True)
+    #AggregatedDate = models.DateTimeField(auto_now = True, null = True)
+    Participation = models.DecimalField(max_digits = 13, decimal_places = 9,
+            verbose_name = "Participation", null = True)
+    LoanType = models.CharField(max_length = 1, choices = LOAN_CHOICES,
+            verbose_name = "Loan Type", null = True)
+    Competitive = models.BooleanField(verbose_name = "Is Competitive")
+    CompetitiveBidRate = models.DecimalField(max_digits = 13, decimal_places = 9,
+            null = True)
+    OrderTiming = models.CharField(max_length = 1, choices = ORDER_CHOICES,
+            verbose_name = "Order Timing")
+    #FundsAvailable = MoneyField(max_digits = 20, decimal_places = 9,
+    #            default_currency = moneyed.USD)
+    FundsAvailable = models.FloatField(verbose_name = "Funds Available")
+    #FundsAvailableDate = models.DateTimeField(auto_now = True)
+    Status = models.CharField(max_length = 1, choices = STATUS_CHOICES,
+            verbose_name = "Status", null = True)
--- Web/bids/views.py
+++ Web/bids/views.py
@@ -1,1 +1,41 @@
-# Create your views here.
+from django.template import RequestContext, loader
+from django.shortcuts import render_to_response, get_object_or_404
+from bids.models import Bid
+from django.http import HttpResponse
+from datetime import datetime
+
+def index(request):
+    context = {}
+    return render_to_response("bids/list_bids.html", context)
+
+def detail(request, bid_id):
+    b = get_object_or_404(Bid, pk=bid_id)
+    return render_to_response('bids/detail.html', {'bid', b},
+        context_instance=RequestContext(request))
+    #HttpResponse("You're detail %s." % bid_id)
+
+def results(request, bid_id):
+    HttpResponse("You're result  %s." % bid_id)
+
+def addbid(request, bid_id):
+    b = get_object_or_404(Bid, pk=bid_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render_to_response('bids/detail.html', {
+            'bid': b,
+            'error_message': "You didn't select a choice.",
+        }, context_instance = RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('bids.views.results', args=(b.id)))
+
+def initial_data(request):
+    b = Bid(User = "1104134",
+            CreatedAt = datetime.strptime("2/1/2012 3:00:00 PM","%m/%d/%Y %H:%M:%S %p").date(),
+            ExpiresAt = datetime.strptime("2/1/2012 3:00:00 PM","%m/%d/%Y %H:%M:%S %p").date(),
+            Type = "G", Aggregated=15000000, Participation = 10, Competitive = False,
+            OrderTiming = "A", FundsAvailable = 187500)
+    b.save()
+    return HttpResponse("Initial data was readed")
