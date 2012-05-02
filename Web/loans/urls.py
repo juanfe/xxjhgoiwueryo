19dd27590c7f2c275eaf92c99ef58d89ba5c1f4c
jjaramillo <jjaramillo@vichara.com>
2012-05-01 20:56:25 -0500

    Adding model's pattern to loan url [ref #7]

--- Web/loans/urls.py
+++ Web/loans/urls.py
@@ -1,10 +1,12 @@
+import os
+
 from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
-urlpatterns = patterns('',
+urlpatterns = patterns('loans.views',
     url(r'^$', 'index'), 
     url(r'^initial_data/$', 'initial_data'),
     #url(r'^(?P<loan_id>\d+)/$', 'loans.views.detail'),
