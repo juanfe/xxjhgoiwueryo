4994f4fa35e868b89a358766f5f0fa4d020321c7
jjaramillo <jjaramillo@vichara.com>
2012-05-04 11:17:25 -0500

    Change space by tabs in settings [ref #7]

--- Web/settings.py
+++ Web/settings.py
@@ -16,15 +16,15 @@ gettext = lambda s: s
 
 LANGUAGE_CODE = 'sr'
 LANGUAGES = (
-    ('en', gettext('English')),
-    #('de', gettext('German')),
-    #('sp', gettext('Spanish')),
+	('en', gettext('English')),
+	#('de', gettext('German')),
+	#('sp', gettext('Spanish')),
 )
 
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 AUTHENTICATION_BACKENDS = (
-    'permission_backend_nonrel.backends.NonrelPermissionBackend',
+	'permission_backend_nonrel.backends.NonrelPermissionBackend',
 )
 
 DOJANGO_DATAGRID_ACCESS = (
@@ -38,43 +38,49 @@ DOJANGO_DATAGRID_ACCESS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.contenttypes',
-    'django.contrib.auth',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'djangotoolbox',
-    'dojango',
-    # It's important to put 'permission_backend_nonrel after djangotoolbox,
-    # because 'permission_backend_nonrel.admin replaces djangotoolbox's User
-    # admin site. 
-    'permission_backend_nonrel',
-    'autoload',
-    'dbindexer',
-    'djmoney',
-    'loans',
-    'bids',
+	'django.contrib.admin',
+	'django.contrib.contenttypes',
+	'django.contrib.auth',
+	'django.contrib.sessions',
+	'django.contrib.staticfiles',
+	'djangotoolbox',
+	'dojango',
+	'datable',
+	# It's important to put 'permission_backend_nonrel after djangotoolbox,
+	# because 'permission_backend_nonrel.admin replaces djangotoolbox's User
+	# admin site. 
+	'permission_backend_nonrel',
+	'autoload',
+	'dbindexer',
+	'djmoney',
+	'loans',
+	'bids',
 	'engine',
-    #'users',
-    # djangoappengine should come last, so it can override a few manage.py commands
-    'djangoappengine',
+	'test_app',
+	#'users',
+	# djangoappengine should come last, so it can override a few manage.py commands
+	'djangoappengine',
+	#'django_iso8601',
+	#'isodate',
+	'iso8601',
+	'xlwt',
 )
 
 MIDDLEWARE_CLASSES = (
-    # This loads the index definitions, so it has to come first
-    'autoload.middleware.AutoloadMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'dojango.middleware.DojoCollector',
+	# This loads the index definitions, so it has to come first
+	'autoload.middleware.AutoloadMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'dojango.middleware.DojoCollector',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-    'django.core.context_processors.media',
-    #'dojango.context_processors.config',
+	'django.contrib.auth.context_processors.auth',
+	'django.core.context_processors.request',
+	'django.core.context_processors.static',
+	'django.core.context_processors.media',
+	#'dojango.context_processors.config',
 )
 
 # This test runner captures stdout and associates tracebacks with their
@@ -82,17 +88,19 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 TEST_RUNNER = 'djangotoolbox.test.CapturingTestSuiteRunner'
 
 ADMIN_MEDIA_PREFIX = '/media/admin/'
-TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),
+		os.path.join(os.path.dirname(__file__),
+				'dojango-datable-test/test_app/templates'),)
 #TEMPLATE_DIRS = (
-#    '/home/juanfe/Vichara/liquidity/Web/templates',
+#	'/home/juanfe/Vichara/liquidity/Web/templates',
 #)
 
 ROOT_URLCONF = 'urls'
 
 #TODO add relative dir as in TEMPLATE_DIRS
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'images'),
-#    "/home/juanfe/Vichara/liquidity/Web/images",
+	os.path.join(os.path.dirname(__file__), 'images'),
+#	"/home/juanfe/Vichara/liquidity/Web/images",
 )
 
 STATIC_URL = '/static/'
@@ -109,9 +117,9 @@ DOJANGO_DOJO_THEME = "claro"
 DOJANGO_DOJO_THEME = "claro" #Available "tundra", "soria", or "nihilo"
 
 #DOJANGO_DOJO_THEME_URL #It defaults to dojo base url/dijit/themes and there
-                        #you can look at the dojo theme structure, e.g.:
-                        #_dojo base url_/dijit/themes/tundra
-                        #_dojo base url_/dijit/themes/tundra/tundra.css
+						#you can look at the dojo theme structure, e.g.:
+						#_dojo base url_/dijit/themes/tundra
+						#_dojo base url_/dijit/themes/tundra/tundra.css
 
 #DOJANGO_DOJO_DEBUG = True
 
