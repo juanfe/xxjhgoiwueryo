c38e449883031813c46c3d547ae5ea314dd04827
jjaramillo <jjaramillo@vichara.com>
2012-05-10 15:10:40 -0500

    Changing by ListLoans in url [ref #7]

--- Web/loans/urls.py
+++ Web/loans/urls.py
@@ -8,7 +8,7 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('loans.views',
     url(r'^$', 'index'), 
-    url(r'^initial_data/$', 'initial_data'),
+    url(r'^loadLoans/$', 'loansModelInstance'),
 	url(r'^listLoans/$', 'ListLoans'),
     #url(r'^(?P<loan_id>\d+)/$', 'loans.views.detail'),
     #url(r'^(?P<loan_id>\d+)/results/$', 'loans.views.results'),
