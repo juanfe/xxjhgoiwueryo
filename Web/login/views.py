7e6a29c7f102c6c23a0d12d43ab365ecc5835649
jjaramillo <jjaramillo@vichara.com>
2012-05-23 16:34:56 -0500

    Add doxygen lines and change the redirect [ref #7]

--- Web/login/views.py
+++ Web/login/views.py
@@ -1,17 +1,17 @@
+## \file login\view.py
+## \brief Views for the Liquidity Spot authentication system
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-#Imports to test login_view
-from django.contrib import auth
-from django.http import HttpResponse
 
 def register(request):
 	if request.method == 'POST':
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
 			new_user = form.save()
-			return HttpResponseRedirect("/accounts/login/")
+			return HttpResponseRedirect("/")
 	else:
 		form = UserCreationForm()
 	return render_to_response("registration/register.html", {'form': form,})
