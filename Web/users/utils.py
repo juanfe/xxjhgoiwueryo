1f06dd45b9402faf7214ca7ab165b800b7b3fdc3
jjaramillo <jjaramillo@vichara.com>
2012-05-24 16:56:08 -0500

    Allow a list o groups in the grand of methods [ref #7]

--- Web/users/utils.py
+++ Web/users/utils.py
@@ -1,11 +1,21 @@
+## \file users\utils.py
+## \brief Utilities to check users qualities
+
 from django.contrib.auth.models import User, Group
 from permission_backend_nonrel.models import UserPermissionList
 
-def UserInGroup(User, groupName):
+def UserInGroup(User, groupList):
+	"""\brief Function to check if a user belong to any of the groups in list.
+
+	\param User to be checked if exist in the list \a groupList
+	\param groupList is a list of group's names
+	"""
 	#TODO allow many check many groups
-	group = Group.objects.get(name=groupName[0])
-	up = UserPermissionList.objects.filter(user = User)
+	gids = []
 	try:
-		return True if unicode(group.id) in up[0].group_fk_list else False
+		for g in Group.objects.filter(name__in=groupList):
+			gids.append(unicode(g.id))
+		up = UserPermissionList.objects.filter(user = User)
+		return True if len(set(gids).intersection(set(up[0].group_fk_list))) > 0 else False
 	except:
 		return False
