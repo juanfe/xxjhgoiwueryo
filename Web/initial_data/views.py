dba98c9b89551c925d6ef442370b8898ede170dd
jjaramillo <jjaramillo@vichara.com>
2012-05-24 15:36:30 -0500

    Adding user to groups by permission_backend_nonrel [ref #7]

--- Web/initial_data/views.py
+++ Web/initial_data/views.py
@@ -3,6 +3,7 @@
 
 from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
+from permission_backend_nonrel import utils
 
 def load_data(request):
 	#Groups of users
@@ -21,31 +22,36 @@ def load_data(request):
 			'Vichara')
 	user_admin.is_staff = True
 	user_admin.save()
-	user_admin.groups.add(admin)
+	#user_admin.groups.add(admin)
+	utils.add_user_to_group(user_admin, admin)
 
 	#Creating Mortgage Operators Users
 	user = User.objects.create_user('ABC Mortgage', 'ABC_Mortgage@tempo.com',
 			'1234')
 	user.is_staff = True
 	user.save()
-	user.groups.add(mo)
+	#user.groups.add(mo)
+	utils.add_user_to_group(user, mo)
 
 	user = User.objects.create_user('Prime Lending', 'Prime_Lending@tempo.com',
 			'1234')
 	user.is_staff = True
 	user.save()
-	user.groups.add(mo)
+	#user.groups.add(mo)
+	utils.add_user_to_group(user, mo)
 
 	user = User.objects.create_user('Best Loans Inc', 'Best_Loans_Inc@tempo.com',
 			'1234')
 	user.is_staff = True
 	user.save()
-	user.groups.add(mo)
+	#user.groups.add(mo)
+	utils.add_user_to_group(user, mo)
 
 	user = User.objects.create_user('Integrity Lending', 'Integrity_Lending@tempo.com',
 			'1234')
 	user.is_staff = True
 	user.save()
-	user.groups.add(mo)
+	#user.groups.add(mo)
+	utils.add_user_to_group(user, mo)
 
 	return HttpResponse("Data had been loaded")
