5759955f5f20d1b7e35d3311e7d2341ac36be486
jjaramillo <jjaramillo@vichara.com>
2012-05-25 14:27:33 -0500

    Change space identation by tabs [ref #7]

--- Web/loans/urls.py
+++ Web/loans/urls.py
@@ -7,10 +7,10 @@ admin.autodiscover()
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('loans.views',
-    url(r'^$', 'index'), 
-    url(r'^loadLoans/$', 'loansModelInstance'),
-    url(r'^listLoans/$', 'ListLoans'),
+	url(r'^$', 'index'), 
+	url(r'^loadLoans/$', 'loansModelInstance'),
+	url(r'^listLoans/$', 'ListLoans'),
 	url(r'^delLoans/$', 'DelLoans'),
-    #url(r'^(?P<loan_id>\d+)/$', 'loans.views.detail'),
-    #url(r'^(?P<loan_id>\d+)/results/$', 'loans.views.results'),
+	#url(r'^(?P<loan_id>\d+)/$', 'loans.views.detail'),
+	#url(r'^(?P<loan_id>\d+)/results/$', 'loans.views.results'),
 )
