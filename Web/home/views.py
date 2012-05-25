ac3be44b1487b2e85e00ff356564f12674d52961
jjaramillo <jjaramillo@vichara.com>
2012-05-25 13:45:57 -0500

    Adding is_logged_in, user an groups to Home view [ref #7]

--- Web/home/views.py
+++ Web/home/views.py
@@ -2,10 +2,15 @@ from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
 from loans.models import Loan, MortgageOriginator
 from django.http import HttpResponse
+from users.utils import is_logged_in, UserName, GetUserGroups
 
 def HomePage(request):
 	try:
-		return render_to_response("home.html",
+		Context = {'is_logged_in': is_logged_in(request)}
+		Context['User'] = UserName(request)
+		Context['Groups'] = GetUserGroups(request)
+		print Context
+		return render_to_response("home.html", Context,
 				context_instance=RequestContext(request))
 	except:
 		return render_to_response("500.html",
