5d2442829e9097c3c1d396199efcd854ee5bc53d
jjaramillo <jjaramillo@vichara.com>
2012-05-26 11:39:07 -0500

    Adding link for generic Bids Form [ref #7]

--- Web/bids/urls.py
+++ Web/bids/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('bids.views',
 	url(r'^$', 'index'),
 	url(r'^initial_data/$', 'initial_data'),
 	url(r'^mybids/$', 'MyBids'),
+	url(r'^bids/$', 'FormBid'),
 	url(r'^addbid/$', 'FormAddBid'),
 )
