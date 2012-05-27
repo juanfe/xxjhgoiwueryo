d46609b8abc4945aa6ab916d9f81ac811b14db4f
jjaramillo <jjaramillo@vichara.com>
2012-05-27 17:42:18 -0500

    Change space by tabs and add group and login checking [ref #7]

--- Web/bids/views.py
+++ Web/bids/views.py
@@ -9,40 +9,40 @@ from users.utils import UserInGroup
 from bids.forms import BidForm
 
 def index(request):
-    context = {}
-    return render_to_response("bids/list_bids.html", context)
+	context = {}
+	return render_to_response("bids/list_bids.html", context)
 
 def detail(request, bid_id):
-    b = get_object_or_404(Bid, pk=bid_id)
-    return render_to_response('bids/detail.html', {'bid', b},
-        context_instance=RequestContext(request))
-    #HttpResponse("You're detail %s." % bid_id)
+	b = get_object_or_404(Bid, pk=bid_id)
+	return render_to_response('bids/detail.html', {'bid', b},
+		context_instance=RequestContext(request))
+	#HttpResponse("You're detail %s." % bid_id)
 
 def results(request, bid_id):
-    HttpResponse("You're result  %s." % bid_id)
+	HttpResponse("You're result  %s." % bid_id)
 
 def addbid(request, bid_id):
-    b = get_object_or_404(Bid, pk=bid_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render_to_response('bids/detail.html', {
-            'bid': b,
-            'error_message': "You didn't select a choice.",
-        }, context_instance = RequestContext(request))
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('bids.views.results', args=(b.id)))
+	b = get_object_or_404(Bid, pk=bid_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render_to_response('bids/detail.html', {
+			'bid': b,
+			'error_message': "You didn't select a choice.",
+		}, context_instance = RequestContext(request))
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('bids.views.results', args=(b.id)))
 
 def initial_data(request):
-    b = Bid(User = "1104134",
-            CreatedAt = datetime.strptime("2/1/2012 3:00:00 PM","%m/%d/%Y %H:%M:%S %p").date(),
-            ExpiresAt = datetime.strptime("2/1/2012 3:00:00 PM","%m/%d/%Y %H:%M:%S %p").date(),
-            Type = "G", Aggregated=15000000, Participation = 10, Competitive = False,
-            OrderTiming = "A", FundsAvailable = 187500)
-    b.save()
-    return HttpResponse("Initial data was readed")
+	b = Bid(User = "1104134",
+			CreatedAt = datetime.strptime("2/1/2012 3:00:00 PM","%m/%d/%Y %H:%M:%S %p").date(),
+			ExpiresAt = datetime.strptime("2/1/2012 3:00:00 PM","%m/%d/%Y %H:%M:%S %p").date(),
+			Type = "G", Aggregated=15000000, Participation = 10, Competitive = False,
+			OrderTiming = "A", FundsAvailable = 187500)
+	b.save()
+	return HttpResponse("Initial data was readed")
 
 def MyBids(request):
 	try:
@@ -68,6 +68,9 @@ def MyBids(request):
 #		return render_to_response("500.html",
 #				context_instance=RequestContext(request))
 
+@login_required
+@user_passes_test(lambda u: UserInGroup(u, ["Admin", "Broker"]),
+		login_url='/accounts/login/?next=/bids/bids/')
 def FormBid(request):
 	latest_bid_list = Bid.objects.all().order_by('User')
 
@@ -75,36 +78,44 @@ def FormBid(request):
 			{'latest_bid_list': latest_bid_list},
 			context_instance=RequestContext(request))
 
+@login_required
+@user_passes_test(lambda u: UserInGroup(u, ["Admin", "Broker"]),
+		login_url='/accounts/login/?next=/bids/bidadd/')
 def FormAddBid(request):
-    # sticks in a POST or renders empty form
-    form = BidForm(request.POST or None)
-    if form.is_valid():
-        cmodel = form.save()
-        #This is where you might chooose to do stuff.
-        #cmodel.name = 'test1'
-        cmodel.save()
-        return redirect(FormBid)
+	# sticks in a POST or renders empty form
+	form = BidForm(request.POST or None)
+	if form.is_valid():
+		cmodel = form.save()
+		#This is where you might chooose to do stuff.
+		#cmodel.name = 'test1'
+		cmodel.save()
+		return redirect(FormBid)
 
-    return render_to_response('bids/formaddbid.html',
-                              {'bid_form': form},
-                              context_instance=RequestContext(request))
+	return render_to_response('bids/formaddbid.html',
+			{'bid_form': form},
+			context_instance=RequestContext(request))
 
+@login_required
+@user_passes_test(lambda u: UserInGroup(u, ["Admin", "Broker"]),
+		login_url='/accounts/login/?next=/bids/bids/')
 def FormEditBid(request, bid_id):
-    bid = get_object_or_404(Bid, pk=bid_id)
-    form = BidForm(request.POST or None, instance=bid)
-    if form.is_valid():
-        bid = form.save()
-        #this is where you might choose to do stuff.
-        #contact.name = 'test'
-        bid.save()
-        return redirect(FormBid)
+	bid = get_object_or_404(Bid, pk=bid_id)
+	form = BidForm(request.POST or None, instance=bid)
+	if form.is_valid():
+		bid = form.save()
+		#this is where you might choose to do stuff.
+		#contact.name = 'test'
+		bid.save()
+		return redirect(FormBid)
 
-    return render_to_response('bids/formeditbid.html',
-                              {'bid_form': form,
-                               'bid_id': bid_id},
-                              context_instance=RequestContext(request))
-                             
+	return render_to_response('bids/formeditbid.html',
+			{'bid_form': form, 'bid_id': bid_id},
+			context_instance=RequestContext(request))
+
+@login_required
+@user_passes_test(lambda u: UserInGroup(u, ["Admin", "Broker"]),
+		login_url='/accounts/login/?next=/bids/bids/')
 def DelBid(request, bid_id):
-    c = Bid.objects.get(pk=bid_id).delete()
+	c = Bid.objects.get(pk=bid_id).delete()
 
-    return redirect(FormBid)
+	return redirect(FormBid)
