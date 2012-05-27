83f80d5c89f5a91d7c604ed189a5dda0b1d7a1f6
jjaramillo <jjaramillo@vichara.com>
2012-05-27 16:36:03 -0500

    Adding urls to del and edit bids [ref #7]

--- Web/bids/urls.py
+++ Web/bids/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('bids.views',
 	url(r'^mybids/$', 'MyBids'),
 	url(r'^bids/$', 'FormBid'),
 	url(r'^bidadd/$', 'FormAddBid'),
+	url(r'^bidedit/(?P<bid_id>\d+)/$$', 'FormEditBid'),
+	url(r'^biddel/(?P<bid_id>\d+)/$', 'DelBid'),
 )
