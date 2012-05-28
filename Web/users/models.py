b4e2a41344850af2193bdfbd36ceb940bb4d5f9c
jjaramillo <jjaramillo@vichara.com>
2012-05-28 09:27:32 -0500

    Adding funds as a float in user and comment the money way [ref #7]

--- Web/users/models.py
+++ Web/users/models.py
@@ -1,10 +1,11 @@
 import moneyed
-from djmoney.models.fields import MoneyField
+#from djmoney.models.fields import MoneyField
 from django.contrib.auth.models import User
 from django.db import models
 
 class FundsCashFlow(models.Model):
 	User = models.ForeignKey(User)
-	Funds = MoneyField(max_digits = 20, decimal_places = 9, default_currency =
-			moneyed.USD)
-	FundsDate = models.DateTimeField(auto_now = True)
+	#Funds = MoneyField(max_digits = 20, decimal_places = 9,
+	#		default_currency = moneyed.USD)
+	#FundsDate = models.DateTimeField(auto_now = True)
+	Funds = models.FloatField(verbose_name = "Funds Available")
