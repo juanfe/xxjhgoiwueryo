7ab13195c91d0feb62562fb05ba4330aafed6238
jjaramillo <jjaramillo@vichara.com>
2012-05-28 10:20:32 -0500

    Adding filter in the User at form FundsForm [ref #7]

--- Web/users/forms.py
+++ Web/users/forms.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 from users.models import FundsCashFlow 
 
 class FundsForm(ModelForm):
 	class Meta:
 		model = FundsCashFlow
+
+	def __init__(self, *args, **kwargs):
+		super (FundsForm, self).__init__(*args, **kwargs)
+		#TODO filter by 'Broker'
+		self.fields['User'].queryset = User.objects.all()
