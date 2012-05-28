fb50702a1e2ba343b95f7fe2b4e9652fbf62edfc
jjaramillo <jjaramillo@vichara.com>
2012-05-28 09:21:55 -0500

    Adding users ass app in settign and urls [ref #7]

--- Web/settings.py
+++ Web/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = (
 	'home',
 	'search',
 	'login',
+	'users',
 	# djangoappengine should come last, so it can override a few manage.py commands
 	'djangoappengine',
 	#'django_iso8601',
