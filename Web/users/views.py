087634e170306823455d2157116b7d92fbcd64e3
jjaramillo <jjaramillo@vichara.com>
2012-05-28 09:28:17 -0500

    Adding funds list view [ref #7]

--- Web/users/views.py
+++ Web/users/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required, user_passes_test
+from users.utils import UserInGroup
+from users.models import FundsCashFlow
+
+@login_required
+@user_passes_test(lambda u: UserInGroup(u, ["Admin"]),
+		login_url='/accounts/login/?next=/bids/bids/')
+def funds(request):
+	users_funds = FundsCashFlow.objects.all()
+
+	return render_to_response('users/formfunds.html',
+			{'users_funds': users_funds},
+			context_instance=RequestContext(request))
