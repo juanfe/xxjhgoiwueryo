e03ab70219fdc0dc914579b785290eadc8986b3b
jjaramillo <jjaramillo@vichara.com>
2012-05-30 10:40:14 -0500

    Adding dataStore and assign to grid [ref #7]

--- Web/engine/dojo-media/scripts/eng.js
+++ Web/engine/dojo-media/scripts/eng.js
@@ -5,12 +5,18 @@ dojo.require("dijit.layout.BorderContainer");
 dojo.require("dijit.layout.ContentPane");
 dojo.require("dojox.layout.ContentPane");
 dojo.require("dojox.grid.DataGrid");
+dojo.require("dojo.data.ItemFileReadStore");
 
 // used for cropping the biography of an actor
 dojo.require("dojox.dtl.filter.strings")
 
 dojo.ready(function(){
+	var dataStore =
+		new dojo.data.ItemFileReadStore(
+			{ data:DataItems }
+		);
 	var grid = dijit.byId("billsGrid");
+	grid.setStore(dataStore);
 	grid.connect(grid, "onRowClick", function(el){
 		var item = this.getItem(el.rowIndex);
 		scripts.eng.showDetails(item);
