8496a5c89f1340ed0e06a31226ee95227569abe4
jjaramillo <jjaramillo@vichara.com>
2012-05-30 12:00:14 -0500

    Change by UserContext way [ref #7]

--- Web/home/views.py
+++ Web/home/views.py
@@ -2,13 +2,11 @@ from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
 from loans.models import Loan, MortgageOriginator
 from django.http import HttpResponse
-from users.utils import is_logged_in, UserName, GetUserGroups
+from users.utils import UserContext 
 
 def HomePage(request):
 	try:
-		Context = {'is_logged_in': is_logged_in(request)}
-		Context['User'] = UserName(request)
-		Context['Groups'] = GetUserGroups(request)
+		Context = UserContext(request)
 		print Context
 		return render_to_response("home.html", Context,
 				context_instance=RequestContext(request))
