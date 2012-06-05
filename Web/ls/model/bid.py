04060ed513fcbd7b4c35982389429632e627f5ce
jjaramillo <jjaramillo@vichara.com>
2012-06-05 11:36:26 -0500

    Adding status and expiresAt to the TestBids [ref #7]

--- Web/ls/model/bid.py
+++ Web/ls/model/bid.py
@@ -41,9 +41,11 @@ class BidsInstance(webapp.RequestHandler):
 				bidtype = 'General',
 				user = getUser('1104134@test.com'),
 				aggregated = 15000000.00,
+				status = 'Active',
 				participation = 10.0,
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("3:00:00 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Auto",
 				orderAt = datetime.datetime.strptime("12/15/11 12:53 PM", "%m/%d/%y %I:%M %p")
 				).put()
@@ -53,10 +55,12 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="3:00:00 PM 1104143",
 				bidtype = 'Specified',
 				user = getUser('1104143@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "MO",
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("3:00:00 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Auto",
 				mo = getUser('ABCD'),
 				orderAt = datetime.datetime.strptime("12/20/11 8:43 AM", "%m/%d/%y %I:%M %p")
@@ -68,10 +72,12 @@ class BidsInstance(webapp.RequestHandler):
 				bidtype = 'General',
 				user = getUser('1104154@test.com'),
 				aggregated = 20000000.00,
+				status = 'Active',
 				participation = 20.0,
 				ordertype = "Competitive",
 				bidrate = 3.0,
 				createdAt = datetime.datetime.strptime("3:00:00 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Auto",
 				orderAt = datetime.datetime.strptime("1/5/12 9:16 AM", "%m/%d/%y %I:%M %p")
 				).put()
@@ -82,11 +88,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="3:00:00 PM 1104152",
 				bidtype = 'Specified',
 				user = getUser('1104152@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "MO",
 				ordertype = "Competitive",
 				bidrate = 3.0,
 				createdAt = datetime.datetime.strptime("3:00:00 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				mo = getUser('BCDE'),
 				ordertiming = "Auto",
 				orderAt = datetime.datetime.strptime("1/5/12 9:15 AM", "%m/%d/%y %I:%M %p")
@@ -98,12 +106,14 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="5:06:12 PM 1104136",
 				bidtype = 'Specified',
 				user = getUser('1104136@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201149669"),
 				ordertype = "Competitive",
 				bidrate = 6.0,
 				createdAt = datetime.datetime.strptime("5:06:12 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -113,10 +123,12 @@ class BidsInstance(webapp.RequestHandler):
 				bidtype = 'General',
 				user = getUser('1104138@test.com'),
 				aggregated = 1000000.00,
+				status = 'Active',
 				participation = 50.0,
 				ordertype = "Competitive",
 				bidrate = 2.125,
 				createdAt = datetime.datetime.strptime("6:32:36 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -126,10 +138,12 @@ class BidsInstance(webapp.RequestHandler):
 				bidtype = 'General',
 				user = getUser('1104139@test.com'),
 				aggregated = 500000.00,
+				status = 'Active',
 				participation = 30.0,
 				ordertype = "Competitive",
 				bidrate = 2.250,
 				createdAt = datetime.datetime.strptime("7:15:48 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -139,12 +153,14 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="7:59:00 PM 1104140",
 				bidtype = 'Specified',
 				user = getUser('1104140@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201148440"),
 				ordertype = "Competitive",
 				bidrate = 2.50,
 				createdAt = datetime.datetime.strptime("7:59:00 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -154,10 +170,12 @@ class BidsInstance(webapp.RequestHandler):
 				bidtype = 'General',
 				user = getUser('1104141@test.com'),
 				aggregated = 500000.00,
+				status = 'Active',
 				participation = 20.0,
 				ordertype = "Competitive",
 				bidrate = 2.50,
 				createdAt = datetime.datetime.strptime("8:42:12 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -167,11 +185,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="9:39:48 PM 1104159",
 				bidtype = 'Specified',
 				user = getUser('1104159@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201148329"),
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("9:39:48 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -181,11 +201,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="11:06:12 PM 1104161",
 				bidtype = 'Specified',
 				user = getUser('1104161@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201149669"),
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("11:06:12 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -195,12 +217,14 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="11:35:00 PM 1104145",
 				bidtype = 'Specified',
 				user = getUser('1104145@test.com'),
+				status = 'Active',
 				participation = 10.0,
 				lorm = "Loan",
 				loan = getLoan("201148329"),
 				ordertype = "Competitive",
 				bidrate = 1.50,
 				createdAt = datetime.datetime.strptime("11:35:00 PM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -210,12 +234,14 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="2:23:00 AM 1104133",
 				bidtype = 'Specified',
 				user = getUser('1104133@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201149669"),
 				ordertype = "Competitive",
 				bidrate = 4.0,
 				createdAt = datetime.datetime.strptime("2:23:00 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -225,11 +251,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="2:27:48 AM 1104149",
 				bidtype = 'Specified',
 				user = getUser('1104149@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201148440"),
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("2:27:48 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -239,11 +267,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="3:54:12 AM 1104151",
 				bidtype = 'Specified',
 				user = getUser('1104151@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201148329"),
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("3:54:12 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -253,9 +283,11 @@ class BidsInstance(webapp.RequestHandler):
 				bidtype = 'General',
 				user = getUser('1104131@test.com'),
 				aggregated = 1000000.00,
+				status = 'Active',
 				participation = 50.0,
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("4:37:24 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -264,11 +296,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="6:47:00 AM 1104155",
 				bidtype = 'Specified',
 				user = getUser('1104155@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "MO",
 				ordertype = "Competitive",
 				bidrate = 2.125,
 				createdAt = datetime.datetime.strptime("6:47:00 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -278,11 +312,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="7:30:12 AM 1104156",
 				bidtype = 'Specified',
 				user = getUser('1104156@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201148009"),
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("7:30:12 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -292,9 +328,11 @@ class BidsInstance(webapp.RequestHandler):
 				bidtype = 'General',
 				user = getUser('1104157@test.com'),
 				aggregated = 1000000.00,
+				status = 'Active',
 				participation = 30.0,
 				ordertype = "Noncompetitive",
 				createdAt = datetime.datetime.strptime("8:13:24 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
 
@@ -304,11 +342,13 @@ class BidsInstance(webapp.RequestHandler):
 		Bid( key_name="8:56:36 AM 1104158",
 				bidtype = 'Specified',
 				user = getUser('1104158@test.com'),
+				status = 'Active',
 				participation = 20.0,
 				lorm = "Loan",
 				loan = getLoan("201148329"),
 				ordertype = "Competitive",
 				bidrate = 2.250,
 				createdAt = datetime.datetime.strptime("8:56:36 AM", "%I:%M:%S %p"),
+				expiresAt = datetime.datetime.strptime("12/15/13 12:53 PM", "%m/%d/%y %I:%M %p"),
 				ordertiming = "Day Trade"
 				).put()
