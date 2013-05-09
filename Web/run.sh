beed1b14b1788d6234d9258ff9a4dc5fede1a8ab
jjaramillo <jjaramillo@vichara.com>
2013-05-09 16:45:47 -0500

    Adding loglevel information

--- Web/run.sh
+++ Web/run.sh
@@ -1,1 +1,7 @@
+#--log_level=...
+#
+#    The lowest logging level at which logging messages will be written to the
+# console; messages of the specified logging level or higher will be output.
+# Possible values are debug, info, warning, error, and critical.
+
 dev_appserver.py .
